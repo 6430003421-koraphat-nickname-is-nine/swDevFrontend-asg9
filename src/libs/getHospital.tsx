a2bf8086321fc02f216d4242ae5a1056e6041873
koraphat <korahat.lum@gmail.com>
2024-10-05 23:55:34 +0700

    update fetch with my own deployed backend

--- src/libs/getHospital.tsx
+++ src/libs/getHospital.tsx
@@ -1,6 +1,7 @@
 export default async function getHospital(hid: string) {
   const response = await fetch(
-    `https://vaccine-app-backend-2.vercel.app/api/v1/hospitals/${hid}`
+    // `https://vaccine-app-backend-2.vercel.app/api/v1/hospitals/${hid}`
+    `https://sw-dev-frontend-asg-backend1-git-main-koraphat2545s-projects.vercel.app/api/v1/hospitals/${hid}`
   );
   if (!response) {
     throw new Error("Failed to fetch hospital id :" + hid);
--- src/libs/getHospitals.tsx
+++ src/libs/getHospitals.tsx
@@ -3,7 +3,8 @@ export default async function getHospitals() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const response = await fetch(
-    "https://vaccine-app-backend-2.vercel.app/api/v1/hospitals"
+    // "https://vaccine-app-backend-2.vercel.app/api/v1/hospitals"
+    "https://sw-dev-frontend-asg-backend1-git-main-koraphat2545s-projects.vercel.app/api/v1/hospitals/"
   );
   if (!response) {
     throw new Error("Failed to fetch hospitals");
